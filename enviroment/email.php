903476eaed9c9e8f9348fe8e905959b29c1c4785
Miguel Costero <miguelcostero@outlook.com>
2017-07-09 20:52:32 -0400

    Reset password backend successfully finished

--- enviroment/email.php
+++ enviroment/email.php
@@ -1,13 +1,32 @@
 <?php
 
-require 'PHPMailerAutoload.php';
+function sendEmail ($email) {
+  require __DIR__ . '/../vendor/autoload.php';
 
-$mail = new PHPMailer;
+  $mail = new PHPMailer;
 
-$mail->isSMTP();
-$mail->Host = 'mail.talentproducciones.com.ve';
-$mail->SMTPAuth = true;
-$mail->Username = 'no-reply@talentproducciones.com.ve';
-$mail->Password = 'T]@L2M1u;?k(';
-$mail->SMTPSecure = 'tls';
-$mail->Port = 465;
+  $mail->SMTPDebug = 2;
+  $mail->isSMTP();
+  $mail->CharSet = 'UTF-8';
+  $mail->SMTPAuth = true;
+
+  $mail->Host = 'mail.talentproducciones.com.ve';
+  // $mail->Port = 465;
+  $mail->Port = 25;
+  $mail->Username = 'no-reply@talentproducciones.com.ve';
+  $mail->Password = 'T]@L2M1u;?k(';
+  // $mail->SMTPSecure = 'tls';
+
+  $mail->setFrom('no-reply@talentproducciones.com.ve', 'no-reply@talentproducciones.com.ve');
+  $mail->isHTML(true);
+  $mail->addAddress($email->address->email, $email->address->name);
+  $mail->Subject = $email->subject;
+  $mail->Body = $email->body;
+  $mail->AltBody = $email->altbody;
+
+  if (!$mail->send()) {
+    return (object) array('status' => 'failed', 'message' => $mail->ErrorInfo);
+  } else {
+    return (object) array('status' => 'success', 'message' => 'Mensaje Enviado');
+  }
+}
--- routes/login.php
+++ routes/login.php
@@ -3,3 +3,11 @@
 $router->map('POST', '/v1/login/', function () {
 	require __DIR__ . '/../controllers/login/login.php';
 });
+
+$router->map('POST', '/v1/reset-password/', function () {
+	require __DIR__ . '/../controllers/login/reset-password.php';
+});
+
+$router->map('POST', '/v1/validate-token/', function () {
+	require __DIR__ . '/../controllers/login/validate-token.php';
+});
